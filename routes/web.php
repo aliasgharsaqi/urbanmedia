37c25cca54d3cf33d4f0895004650debf7b2c1a5
ali <coder.ali.381.com>
2025-07-03 03:49:35 -0700

    updation

--- routes/web.php
+++ routes/web.php
@@ -31,75 +31,6 @@ Route::middleware('check.userAuthCheck')->group(function () {
     Route::get('/run-commands',[UserController::class, 'runMigrations']);
     Route::match(['post', 'get'], '/admins/{id?}',                      [UserController::class, 'staff'])->name('staff');
     Route::match(['post', 'get'], '/users',                             [UserController::class, 'users'])->name('users');
-    Route::match(['post', 'get'], '/vfs/embassy',                       [UserController::class, 'vfs_embassy'])->name('vfs.embassy');
-    Route::match(['post', 'get'], '/settings/{user_id?}',               [UserController::class, 'settings'])->name('settings');
-    Route::match(['post', 'get'], '/currencies',                        [UserController::class, 'currencies'])->name('currencies');
-    Route::match(['post', 'get'], '/categories',                        [UserController::class, 'categories'])->name('categories');
-    Route::match(['post', 'get'], '/status',                            [UserController::class, 'software_status'])->name('software.status');
-
-    Route::match(['post', 'get'], '/countries',                         [UserController::class, 'countries'])->name('countries');
-    Route::match(['post', 'get'], '/blank-temp',                        [UserController::class, 'blank_temp'])->name('blank.temp');
-    Route::match(['post', 'get'], '/add-blank',                         [UserController::class, 'add_blank'])->name('add.blank');
-    Route::match(['post', 'get'], '/locations',                         [UserController::class, 'locations'])->name('locations');
-    Route::match(['post', 'get'], '/staff/add/{id?}',                   [UserController::class, 'add'])->name('staff.add');
-    Route::match(['post', 'get'], '/staff/store',                       [UserController::class, 'store'])->name('staff.store');
-    Route::match(['post', 'get'], '/staff/delete/{id?}',                [UserController::class, 'delete'])->name('staff.delete');
-    Route::match(['post', 'get'], '/staff/{id}',                        [UserController::class, 'staff_detail_page'])->name('staff.detail');
-    Route::match(['post', 'get'], '/passport/expiry',                   [UserController::class, 'passport_expiry'])->name('passport.expiry');
-    Route::match(['post', 'get'], '/alerts/fetch',                      [UserController::class, 'fetchUnseenAlerts'])->name('alerts.fetch');
-    Route::match(['post', 'get'], '/update/alert/status',               [UserController::class, 'updateStatus']);
-    Route::match(['post', 'get'], '/delete/alert',                      [UserController::class, 'alert_delete']);
-
-
-
-    Route::match(['post', 'get'], '/application/add/{id?}',             [ApplicationController::class, 'add'])->name('application.add');
-    Route::match(['post', 'get'], '/application/store',                 [ApplicationController::class, 'application_store'])->name('application.store');
-    Route::match(['post', 'get'], '/application',                       [ApplicationController::class, 'index'])->name('application.index');
-    Route::match(['post', 'get'], '/application/delete/{id}',           [ApplicationController::class, 'delete'])->name('application.delete');
-    Route::match(['post', 'get'], '/application/{id}',                  [ApplicationController::class, 'detail_page'])->name('application.detail');
-
-Route::match(['post', 'get'], '/tracking/application/add/{id?}',             [TrackingApplicationController::class, 'add'])->name('tracking.application.add');
-Route::match(['post', 'get'], '/tracking/application/store',                 [TrackingApplicationController::class, 'application_store'])->name('tracking.application.store');
-Route::match(['post', 'get'], '/tracking/application',                       [TrackingApplicationController::class, 'index'])->name('tracking.application.index');
-Route::match(['post', 'get'], '/tracking/application/delete/{id}',           [TrackingApplicationController::class, 'delete'])->name('tracking.application.delete');
-Route::match(['post', 'get'], '/tracking/application/{id}',                  [TrackingApplicationController::class, 'detail_page'])->name('tracking.application.detail');
-
-    Route::match(['post', 'get'], '/appointment/schedule/add/{id?}',    [ScheduleController::class, 'add'])->name('schedule.appointment.add');
-    Route::match(['post', 'get'], '/appointment/schedule',              [ScheduleController::class, 'schedule_index'])->name('schedule.appointment.index');
-    Route::match(['post', 'get'], '/appointment/schedule/store',        [ScheduleController::class, 'appointment_store'])->name('schedule.appointment.store');
-    Route::match(['post', 'get'], '/appointment/schedule/delete/{id}',  [ScheduleController::class, 'delete'])->name('schedule.appointment.delete');
-    Route::match(['post', 'get'], '/appointment/schedule/{id}',         [ScheduleController::class, 'schedule_detail_page'])->name('schedule.detail');
-
-    Route::match(['post', 'get'], '/appointment/pending/add/{id?}',    [PendingController::class, 'add'])->name('pending.appointment.add');
-    Route::match(['post', 'get'], '/appointment/pending',              [PendingController::class, 'pending_index'])->name('pending.appointment.index');
-    Route::match(['post', 'get'], '/appointment/pending/store',        [PendingController::class, 'appointment_store'])->name('pending.appointment.store');
-    Route::match(['post', 'get'], '/appointment/pending/delete/{id}',  [PendingController::class, 'delete'])->name('pending.appointment.delete');
-    Route::match(['post', 'get'], '/appointment/pending/{id}',         [PendingController::class, 'pending_detail_page'])->name('pending.detail');
-
-
-    Route::match(['post', 'get'], '/insurance/add/{id?}',             [InsuranceController::class, 'add'])->name('insurance.add');
-    Route::match(['post', 'get'], '/insurance',                       [InsuranceController::class, 'index'])->name('insurance.index');
-    Route::match(['post', 'get'], '/insurance/store',                 [InsuranceController::class, 'store'])->name('insurance.store');
-    Route::match(['post', 'get'], '/insurance/delete/{id?}',          [InsuranceController::class, 'delete'])->name('insurance.delete');
-    Route::match(['post', 'get'], '/insurance/{id}',                  [InsuranceController::class, 'insurance_detail_page'])->name('schedule.detail');
-
-    Route::match(['post', 'get'], '/tracking/add',                     [TrackingController::class, 'add'])->name('tracking.add');
-    Route::match(['post', 'get'], '/tracking',                         [TrackingController::class, 'index'])->name('tracking.index');
-    
-    Route::match(['post', 'get'], '/hotel/add/{id?}',                  [HotelBookingController::class, 'add'])->name('hotel.add');
-    Route::match(['post', 'get'], '/hotel',                            [HotelBookingController::class, 'index'])->name('hotel.index');
-    Route::match(['post', 'get'], '/hotel/store',                       [HotelBookingController::class, 'store'])->name('hotel.store');
-    Route::match(['post', 'get'], '/hotel/delete/{id?}',                [HotelBookingController::class, 'delete'])->name('hotel.delete');
-    Route::match(['post', 'get'], '/hotel/booking/{id}',                [HotelBookingController::class, 'hotel_detail_page'])->name('hotel.detail');
-   
-
-
-    Route::match(['post', 'get'], '/ds/add/{id?}',                      [DSController::class, 'add'])->name('ds.add');
-    Route::match(['post', 'get'], '/ds',                                [DSController::class, 'index'])->name('ds.index');
-    Route::match(['post', 'get'], '/ds/store/{id?}',                    [DSController::class, 'store'])->name('ds.store');
-    Route::match(['post', 'get'], '/ds/delete/{id?}',                   [DSController::class, 'delete'])->name('ds.delete');
-    Route::match(['post', 'get'], '/ds/{id}',                           [DSController::class, 'ds_detail_page'])->name('ds.detail');
-
     
     Route::match(['post', 'get'], '/client/add/{id?}',                  [ClientController::class, 'add'])->name('client.add');
     Route::match(['post', 'get'], '/client',                            [ClientController::class, 'index'])->name('client.index');
@@ -123,22 +54,11 @@ Route::match(['post', 'get'], '/register',        [AuthController::class, 'user_
 Route::match(['post', 'get'], '/logout',          [AuthController::class, 'logout'])->name('logout');
 Route::match(['post', 'get'], '/verify/{hash}',   [AuthController::class, 'verify'])->name('verify');
 
-Route::get('/forget',function(){
 
-    return view('pages.auth.otp');
-});
-Route::get('/password',function(){
 
-    return view('pages.auth.password');
-});
+Route::get('/dashboard',function(){
 
-Route::get('/detail',function(){
-
-    return view('pages.client.add');
-});
-Route::get('/d',function(){
-
-    return view('pages.client.alert');
+    return view('pages.dashboard');
 });
 
 
