af426a566b54db6071e3b7f59fd7f1378258de2e
ali <coder.ali.381.com>
2025-07-03 05:08:35 -0700

    dashboard design complete

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,15 @@
+
+<nav class=" bg-white shadow-md px-6 py-4 flex items-center justify-between">
+      <!-- Logo -->
+      <div class="flex items-center">
+          <img src="/assets/images/logo.webp" alt="Logo" class="h-14 w-auto" />
+      </div>
+
+      <!-- Button -->
+      <div>
+          <button
+              class="bg-orange-500 hover:bg-orange-600 text-white font-semibold px-6 py-2 rounded-lg transition duration-200">
+              Get Started
+          </button>
+      </div>
+  </nav>
